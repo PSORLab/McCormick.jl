49e2f811538e65aa35d2413c1aac634560604a1a
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2020-05-01 13:21:14 -0400

    Tidy operations and code optimization

--- src/forward_operators/concave_increasing.jl
+++ src/forward_operators/concave_increasing.jl
@@ -22,16 +22,18 @@ for opMC in (:log, :log2, :log10, :log1p, :acosh, :sqrt)
                xU = x.Intv.hi
                xLc = z.lo
                xUc = z.hi
-               midcc, cc_id = mid3(x.cc, x.cv ,xU)
-               midcv, cv_id = mid3(x.cc, x.cv ,xL)
-               dcv = (xUc > xLc) ? (xUc - xLc)/(xU - xL) : 0.0
-               if (xU - xL) == 0.0
+               midcc, cc_id = mid3(x.cc, x.cv, xU)
+               midcv, cv_id = mid3(x.cc, x.cv, xL)
+               deltaX = (xU - xL)
+               slope = (xUc - xLc)/deltaX
+               dcv = (xUc > xLc) ? slope : 0.0
+               if deltaX == 0.0
                   convex = xLc
                else
-                  convex = xLc*((xU - midcv)/(xU - xL)) + xUc*((midcv - xL)/(xU - xL))
+                  convex = (xLc*(xU - midcv) + xUc*(midcv - xL))/deltaX
                end
                concave = ($opMC)(midcc)
-               convex_grad = ((xUc - xLc)/(xU - xL))*x.cv_grad
+               convex_grad = slope*x.cv_grad
                concave_grad = ($dop)*x.cc_grad
                return MC{N, Diff}(convex, concave, z, convex_grad, concave_grad, x.cnst)
          end
