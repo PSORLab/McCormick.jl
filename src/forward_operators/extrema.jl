004c40c8fdb7bc568f4b69a25b99e2db76f13b14
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2020-05-01 15:55:42 -0400

    Update min/max Diff

--- src/forward_operators/extrema.jl
+++ src/forward_operators/extrema.jl
@@ -1,3 +1,16 @@
+# Copyright (c) 2018: Matthew Wilhelm & Matthew Stuber.
+# This work is licensed under the Creative Commons Attribution-NonCommercial-
+# ShareAlike 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+#############################################################################
+# McCormick.jl
+# A McCormick operator library in Julia
+# See https://github.com/PSORLab/McCormick.jl
+#############################################################################
+# src/forward_operators/benchmarks.jl
+# Defines univariate and bivariate max and min operators.
+#############################################################################
 
 # Univariate min/max operators
 @inline deriv_max(x::Float64, c::Float64) = (x < c) ? 0.0 : 1.0
@@ -39,74 +52,79 @@ end
 
 # Multivariate min/max operators
 @inline function psil_max(x::Float64, y::Float64, lambda::Interval{Float64}, nu::Interval{Float64}, f1::MC{N, Diff}, f2::MC{N, Diff}) where N
-   if (nu.hi <= lambda.lo)
-     val = x
-   elseif (lambda.hi <= nu.lo)
-     val = y
-   elseif ((nu.lo <= lambda.lo) && (lambda.lo < nu.hi))
-     val = x + (nu.hi - lambda.lo)*max(0.0, (y - x)/(nu.hi - lambda.lo))^(MC_DIFF_MU+1)
-   else
-     val = y + (lambda.hi - nu.lo)*max(0.0, (x-y)/(lambda.hi-nu.lo))^(MC_DIFF_MU+1)
-   end
-   if (nu.hi <= lambda.lo)
-     grad_val = f1.cv_grad
-   elseif (lambda.hi <= nu.lo)
-     grad_val = f1.cc_grad
-   else
-     grad_val = max(0.0,psil_max_dx(x,y,lambda,nu))*f1.cv_grad +
-                min(0.0,psil_max_dx(x,y,lambda,nu))*f1.cc_grad +
-                max(0.0,psil_max_dy(x,y,lambda,nu))*f2.cv_grad +
-                min(0.0,psil_max_dy(x,y,lambda,nu))*f2.cc_grad
-   end
-   return val, grad_val
+    if (nu.hi <= lambda.lo)
+        val = x
+    elseif (lambda.hi <= nu.lo)
+        val = y
+    elseif ((nu.lo <= lambda.lo) && (lambda.lo < nu.hi))
+        val = x + (nu.hi - lambda.lo)*max(0.0, (y - x)/(nu.hi - lambda.lo))^MC_DIFF_MU1
+    else
+        val = y + (lambda.hi - nu.lo)*max(0.0, (x - y)/(lambda.hi - nu.lo))^MC_DIFF_MU1
+    end
+    if (nu.hi <= lambda.lo)
+        grad_val = f1.cv_grad
+    elseif (lambda.hi <= nu.lo)
+        grad_val = f1.cv_grad
+    else
+        grad_val = max(0.0, psil_max_dx(x, y, lambda, nu))*f1.cv_grad +
+                   min(0.0, psil_max_dx(x, y, lambda, nu))*f1.cc_grad +
+                   max(0.0, psil_max_dy(x, y, lambda, nu))*f2.cv_grad +
+                   min(0.0, psil_max_dy(x, y, lambda, nu))*f2.cc_grad
+    end
+    return val, grad_val
 end
-@inline function thetar(x::Float64, y::Float64, lambda::Interval{Float64}, nu::Interval{Float64})
-    return (max(lambda.lo,nu.lo) + max(lambda.hi,nu.hi)-max(lambda.lo,nu.hi) +
-    max(lambda.hi,nu.lo))*max(0.0,((lambda.hi-x)/(lambda.hi-lambda.lo)-(y-nu.lo)/(nu.hi-nu.lo)))^(MC_DIFF_MU+1)
-end
+
 @inline function psil_max_dx(x::Float64, y::Float64, lambda::Interval{Float64}, nu::Interval{Float64})
-  (nu.lo <= lambda.lo < nu.hi) && (1.0-(MC_DIFF_MU+1)*max(0.0,(y-x)/(nu.hi-lambda.lo))^MC_DIFF_MU)
-  return (MC_DIFF_MU+1)*max(0.0,(x-y)/(lambda.hi-nu.lo))^MC_DIFF_MU
+    if in(lambda.lo, nu)
+        return 1.0 - MC_DIFF_MU1*max(0.0, (y - x)/(nu.hi - lambda.lo))^MC_DIFF_MU
+    end
+    return MC_DIFF_MU1*max(0.0, (x - y)/(lambda.hi - nu.lo))^MC_DIFF_MU
 end
 @inline function psil_max_dy(x::Float64, y::Float64, lambda::Interval{Float64}, nu::Interval{Float64})
-  (nu.lo <= lambda.lo < nu.hi) && (return (MC_DIFF_MU+1)*max(0.0,(y-x)/(nu.hi-lambda.lo))^MC_DIFF_MU)
-  return 1.0-(MC_DIFF_MU+1)*max(0.0,(x-y)/(lambda.hi-nu.lo))^MC_DIFF_MU
+    if in(lambda.lo, nu)
+        return MC_DIFF_MU1*max(0.0, (y - x)/(nu.hi - lambda.lo))^MC_DIFF_MU
+    end
+    return 1.0 - MC_DIFF_MU1*max(0.0, (x - y)/(lambda.hi - nu.lo))^MC_DIFF_MU
 end
 @inline function psir_max(x::Float64, y::Float64, xgrad::SVector{N,Float64}, ygrad::SVector{N,Float64},
-                  lambda::Interval{Float64}, nu::Interval{Float64}) where N
-    if (nu.hi <= lambda.lo)
-      return x,xgrad
-    elseif (lambda.hi <= nu.lo)
-      return y,ygrad
-    else
-      val = max(lambda.hi,nu.hi)-(max(lambda.hi,nu.hi)-max(lambda.lo,nu.hi))*
-          ((lambda.hi-x)/(lambda.hi-lambda.lo))-
-          (max(lambda.hi,nu.hi)-max(lambda.hi,nu.lo))*((nu.hi-y)/(nu.hi-nu.lo))+thetar(x,y,nu,lambda)
-      grad_val = (max(lambda.hi,nu.hi)-max(lambda.lo,nu.hi))/(lambda.hi-lambda.lo)*xgrad +
-                 (max(lambda.hi,nu.hi)-max(nu.lo,lambda.hi))/(nu.hi-nu.lo)*ygrad +
-                 (MC_DIFF_MU+1)*(max(lambda.hi,nu.lo)+max(lambda.lo,nu.hi)-max(lambda.lo,nu.lo)-max(lambda.hi,nu.hi))*
-                 max(0.0,((lambda.hi-x)/(lambda.hi-lambda.lo))-((y-nu.lo)/(nu.hi-nu.lo)))^MC_DIFF_MU*
-                 ((1.0/(lambda.hi-lambda.lo))*xgrad+(1.0/(nu.hi-nu.lo))*ygrad)
-      return val, grad_val
-    end
+                          lambda::Interval{Float64}, nu::Interval{Float64}) where N
+
+    (nu.hi <= lambda.lo) && (return (x, xgrad))
+    (lambda.hi <= nu.lo) && (return (y, ygrad))
+
+    maxUU = max(lambda.hi, nu.hi)
+    maxLU = max(lambda.lo, nu.hi)
+    maxUL = max(lambda.hi, nu.lo)
+    maxLL = max(lambda.lo, nu.lo)
+
+    diamx = diam(lambda)
+    diamy = diam(nu)
+    delta = maxLL + maxUU - maxLU - maxUL
+    theta_arg = max(0.0, (lambda.hi - x)diamx - (y - nu.lo)/diamy)
+    thetar = delta*theta_arg^MC_DIFF_MU1
+
+    coeff1 = maxUU - maxLU
+    coeff2 = maxUU - maxUL
+    val = maxUU - coeff1*(lambda.hi-x)/diamx - coeff2*(nu.hi - y)/diamy
+
+    shared_term = -MC_DIFF_MU1T*delta*theta_arg^MC_DIFF_MU
+    grad_val = ((coeff1 + shared_term)/diamx)*xgrad + ((coeff2 + shared_term)/diamy)*ygrad
+    return val, grad_val
 end
 @inline function max_kernel(x::MC{N, Diff}, y::MC{N, Diff}, z::Interval{Float64}) where N
-    maxxL = z.lo
-    maxxU = z.hi
-    cc = 0.0
-    cv = 0.0
-    temp_mid = 0.0
+
     if (y.Intv.hi <= x.Intv.lo) || (x.Intv.hi <= y.Intv.lo)
-        cv,cv_grad = psil_max(x.cv, y.cv, x.Intv, y.Intv, x, y)
+        cv, cv_grad = psil_max(x.cv, y.cv, x.Intv, y.Intv, x, y)
     elseif (y.Intv.lo <= x.Intv.lo) & (x.Intv.lo < y.Intv.hi)
-        temp_mid = mid3v(x.cv, x.cc, y.cv - (y.Intv.hi - x.Intv.lo)*(MC_DIFF_MU+1)^(-1/MC_DIFF_MU))
-        cv,cv_grad = psil_max(temp_mid, y.cv, x.Intv, y.Intv, x, y)
+        temp = mid3v(x.cv, x.cc, y.cv - (y.Intv.hi - x.Intv.lo)*MC_DIFF_DIV)
+        cv, cv_grad = psil_max(temp, y.cv, x.Intv, y.Intv, x, y)
     elseif (x.Intv.lo < y.Intv.lo) & (y.Intv.lo < x.Intv.hi)
-        temp_mid = mid3v(y.cv, y.cc, x.cv - (x.Intv.hi - y.Intv.lo)*(MC_DIFF_MU+1)^(-1/MC_DIFF_MU))
-        cv,cv_grad = psil_max(x.cv, temp_mid, x.Intv, y.Intv, x, y)
+        temp = mid3v(y.cv, y.cc, x.cv - (x.Intv.hi - y.Intv.lo)*MC_DIFF_DIV)
+        cv, cv_grad = psil_max(x.cv, temp, x.Intv, y.Intv, x, y)
     end
-    cc,cc_grad = psir_max(x.cc, y.cc, x.cc_grad, y.cv_grad, x.Intv, y.Intv)
-    return MC{N, Diff}(cv, cc, max(x.Intv, y.Intv), cv_grad, cc_grad, (x.cnst && y.cnst))
+
+    cc, cc_grad = psir_max(x.cc, y.cc, x.cc_grad, y.cv_grad, x.Intv, y.Intv)
+    return MC{N, Diff}(cv, cc, z, cv_grad, cc_grad, (x.cnst && y.cnst))
 end
 
 @inline function max_kernel(x::MC{N, MV}, y::MC{N, MV}, z::Interval{Float64}) where N
