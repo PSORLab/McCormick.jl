641dae5bbe29f4e52253eb7b2592e36d0aba6dc0
Matt W <matthew.wilhelm@uconn.edu>
2020-09-29 15:31:00 -0400

    Fix Swish concave relaxation cases 4-6

--- src/forward_operators/activation_functions.jl
+++ src/forward_operators/activation_functions.jl
@@ -461,27 +461,39 @@ end
 @inline function swish1_env(x::Float64, y::Float64, z::Float64)
     (x - y) - (swish1(x) - swish1(y))/swish1_deriv(x)
 end
+
+@inline function swish_rt1(x::Float64, y::Float64, z::Float64)
+    swish1_deriv(y)*(x - y) + swish1(y) - swish1(x)
+end
+
+@inline function swish_rt1_deriv(x::Float64, y::Float64, z::Float64)
+    swish1_deriv(y) - swish1_deriv(x)
+end
+
 @inline function cc_swish1(x::Float64, xL::Float64, xU::Float64, p1::Float64, p2::Float64)
     # Single convexity regions
     if xL >= SWISH1_2D_ROOT2
-        @show "CASE1"
         return swish1(x), swish1_deriv(x), p1, p2
     elseif xU <= SWISH1_2D_ROOT1
-        @show "CASE2"
-        @show swish1(x)
         return swish1(x), swish1_deriv(x), p1, p2
     elseif (SWISH1_2D_ROOT1 <= xL) && (xU <= SWISH1_2D_ROOT2)
-        blarg = dline_seg(swish1, swish1_deriv, x, xL, xU)..., p1, p2
-        @show "CASE3", blarg
-        return blarg
+        return dline_seg(swish1, swish1_deriv, x, xL, xU)..., p1, p2
     end
 
-    if p1 === Inf
-        p1, flag = secant(0.0, xU, 0.0, xU, swish1_env, xL, 0.0)
-        flag && (p1 = golden_section(0.0, xU, swish1_env, xL, 0.0))
+    if xL < SWISH1_2D_ROOT1
+        xUstar, flag = newton(0.0, SWISH1_MIN, 0.0, swish_rt1, swish_rt1_deriv, xL, 0.0)
+        flag && (xUstar = golden_section(SWISH1_2D_ROOT1, 0.0, swish_rt1, xL, 0.0))
+        if xUstar > xU
+            if p1 === Inf
+                p1, flag = secant(xL, SWISH1_2D_ROOT1, xL, SWISH1_2D_ROOT1, swish1_env, xU, 0.0)
+                flag && (p1 = golden_section(0.0, xU, swish1_env, xL, 0.0))
+            end
+            (x >= p1) && (return dline_seg(swish1, swish1_deriv, x, p1, xU)..., p1, p2)
+            return swish1(x), swish1_deriv(x), p1, p2
+        else
+            return dline_seg(swish1, swish1_deriv, x, xL, xU)..., p1, p2
+        end
     end
-    (x <= p1) && (return dline_seg(swish1, swish1_deriv, x, xL, p1)..., p1, p2)
-    return swish1(x), swish1_deriv(x), p1, p2
 end
 @inline function cv_swish1(x::Float64, xL::Float64, xU::Float64, p1::Float64, p2::Float64)
     # Single convexity regions
@@ -500,6 +512,7 @@ end
     #end
     #(x <= p) && (return swish1(x), swish1_deriv(x), p1, p2)
     #return dline_seg(swish1, swish1_deriv, x, p, xU)..., p1, p2
+    return swish1(x), swish1_deriv(x), p1, p2
 end
 
 # define kernel and operator for sigmoid, bisigmoid, softsign, gelu
