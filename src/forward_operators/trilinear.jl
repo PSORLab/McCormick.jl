a3528758279bcdb782637577c157aefaefb2792a
Matt W <matthew.wilhelm@uconn.edu>
2020-10-20 14:54:15 -0400

    Start on trilinear case 2

--- src/forward_operators/trilinear.jl
+++ src/forward_operators/trilinear.jl
@@ -97,8 +97,13 @@ end
 function is_tri_case_1(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}) where {N,T<:RelaxTag}
     (lo(x) >= 0.0) && (lo(y) >= 0.0) && (hi(z) >= 0.0) && (lo(z) <= 0.0)
 end
+function is_tri_case_2(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}) where {N,T<:RelaxTag}
+    (lo(x) >= 0.0) && (lo(y) <= 0.0) && (lo(z) <= 0.0) && (hi(y) >= 0.0) && (hi(z) >= 0.0)
+end
 
 """
+trilinear_case_1
+
 Case 3.1 + Case 4.1 of Meyer-Floudas 2004
 """
 function trilinear_case_1(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float64}) where {N,T<:RelaxTag}
@@ -149,11 +154,75 @@ function trilinear_case_1(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
     @unpack_trilinear_end()
 end
 
+
+"""
+trilinear_case_2
+
+Case 3.2 + Case 4.2 of Meyer-Floudas 2004
+"""
+function trilinear_case_2(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float64}) where {N,T<:RelaxTag}
+    @unpack_trilinear_bnd()
+
+    #delZ = zU - zL
+    #θcv = xyzLUU - xyzUUL - xyzLLU + xyzULU
+    #θcc = xyzULL - xyzUUU - xyzLLL + xyzLUL
+
+    # define cv and coefficients
+    #cv_b1 = -2.0*xyzUUU
+    #cv_b2 = -(xyzLUL + xyzLUU)
+    #cv_b3 = -(xyzLUL + xyzLLL)
+    #cv_b4 = -(xyzULU + xyzULL)
+    #cv_b5 = -(xyzULL + xyzLLL)
+    #cv_b6 = -θcv*zL/delZ - xyzLUU - xyzULL + xyzUUL
+
+    #cv_ax1, cv_ax2, cv_ax3, cv_ax4, cv_ax5, cv_ax6 = yzUU, yzUL, yzUL, yzLU, yzLL, yzLU
+    #cv_ay1, cv_ay2, cv_ay3, cv_ay4, cv_ay5, cv_ay6 = xzUU, xzLU, xzLL, xzUL, xzLL, xzLU
+    #cv_az1, cv_az2, cv_az3, cv_az4, cv_az5, cv_az6 = xyUU, xyLU, xyLL, xyUL, xyLL, θcv/delZ
+
+    # define cc and coefficients
+    #cc_b1 = -2.0*xyzUUL
+    #cc_b2 = -(xyzULU + xyzULL)
+    #cc_b3 = -(xyzLUU + xyzLLU)
+    #cc_b4 = -(xyzLUU + xyzLUL)
+    #cc_b5 = -(xyzULU + xyzLLU)
+    #cc_b6 = θcc*zU/delZ - xyzULL - xyzLUL + xyzUUU
+
+    #cc_ax1, cc_ax2, cc_ax3, cc_ax4, cc_ax5, cc_ax6 = yzUL, yzLL, yzUU, yzUU, yzLU, yzLU
+    #cc_ay1, cc_ay2, cc_ay3, cc_ay4, cc_ay5, cc_ay6 = xzUL, xzUU, xzLU, xzLL, xzUU, xzLU
+    #cc_az1, cc_az2, cc_az3, cc_az4, cc_az5, cc_az6 = xyUU, xyUL, xyLL, xyLU, xyLL, θcc/delZ
+
+    #cv1 = cv_ax1*x.cv + cv_ay1*y.cv + cv_az1*z.cv + cv_b1
+    #cv2 = cv_ax2*x.cv + cv_ay2*y.cv + cv_az2*z.cc + cv_b2
+    #cv3 = cv_ax3*x.cv + cv_ay3*y.cc + cv_az3*z.cc + cv_b3
+    #cv4 = cv_ax4*x.cv + cv_ay4*y.cc + cv_az4*z.cv + cv_b4
+    #cv5 = cv_ax5*x.cv + cv_ay5*y.cc + cv_az5*z.cc + cv_b5
+    #cv6 = cv_ax6*x.cv + cv_ay6*y.cv + cv_az6*ifelse(cv_az6 > 0.0, z.cv, z.cv) + cv_b6
+
+    #cc1 = cc_ax1*x.cc + cc_ay1*y.cc + cc_az1*z.cc + cc_b1
+    #cc2 = cc_ax2*x.cc + cc_ay2*y.cc + cc_az2*z.cv + cc_b2
+    #cc3 = cc_ax3*x.cc + cc_ay3*y.cc + cc_az3*z.cv + cc_b3
+    #cc4 = cc_ax4*x.cc + cc_ay4*y.cc + cc_az4*z.cc + cc_b4
+    #cc5 = cc_ax5*x.cc + cc_ay5*y.cc + cc_az5*z.cv + cc_b5
+    #cc6 = cc_ax6*x.cc + cc_ay6*y.cc + cc_az6*ifelse(cc_az6 > 0.0, z.cc, z.cv) + cc_b6
+
+    @unpack_trilinear_end()
+end
+
 x_mul_y2(x, y) = x*y^2
 
 function mult_kernel(x1::MC{N,T}, x2::MC{N,T}, x3::MC{N,T}, z::Interval{Float64}) where {N, T<:Union{NS,MV}}
-	is_tri_case_1(x1, x2, x3) && trilinear_case_1(x1, x2, x3, z)
-	x1*x2*x3
+	if x1 == x2
+		if x1 == x3
+			return x1^3
+		else
+			return x_mul_y2(x3, x1)
+		end
+	elseif x2 == x3
+		return x_mul_y2(x1, x2)
+	end
+	is_tri_case_1(x1, x2, x3) && return trilinear_case_1(x1, x2, x3, z)
+    is_tri_case_2(x1, x2, x3) && return x1*x2*x3
+	return x1*x2*x3
 end
 
 @inline function *(x1::MC{N,T}, x2::MC{N,T}, x3::MC{N,T}) where {N, T<:Union{NS,MV}}
@@ -168,5 +237,5 @@ end
 	else
 		z = x1.Intv*x2.Intv*x3.Intv
 	end
-	mult_kernel(x1, x2, x3, z)
+	return mult_kernel(x1, x2, x3, z)
 end
