4b4a7fc47e6881e59316c32ebb524d5009fad577
Matt W <matthew.wilhelm@uconn.edu>
2020-10-28 11:55:25 -0400

    Add trilinear case 9, concave

--- src/forward_operators/trilinear.jl
+++ src/forward_operators/trilinear.jl
@@ -164,7 +164,6 @@ function trilinear_case_1(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
     @unpack_trilinear_end()
 end
 
-
 """
 trilinear_case_2
 
@@ -281,7 +280,7 @@ function trilinear_case_9(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
 
     delX = xU - xL
     θcv = xyzUUL - xyzLLL - xyzUUU + xyzULU
-    #θcc = xyzLUL - xyzULL - xyzLUU + xyzUUU
+    θcc = xyzLUL - xyzULL - xyzLUU + xyzLLU
 
     # define cv and coefficients
     cv_b1 = -2.0*xyzULL
@@ -296,16 +295,16 @@ function trilinear_case_9(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
     cv_az1, cv_az2, cv_az3, cv_az4, cv_az5, cv_az6 = xyUL, xyLU, xyLL, xyLL, xyUU, xyUU
 
     # define cc and coefficients
-    #cc_b1 = -2.0*xyzUUL
-    #cc_b2 = -(xyzUUU + xyzULU)
-    #cc_b3 = -(xyzLUL + xyzLLL)
-    #cc_b4 = -(xyzLLU + xyzLLL)
-    #cc_b5 = -(xyzLLU + xyzULU)
-    #cc_b6 = -θcc*yL/delY - xyzLUL - xyzUUU + xyzULL
+    cc_b1 = -2.0*xyzLLL
+    cc_b2 = -(xyzLLU + xyzULU)
+    cc_b3 = -(xyzUUU + xyzULU)
+    cc_b4 = -(xyzUUU + xyzUUL)
+    cc_b5 = -(xyzLUL + xyzUUL)
+    cc_b6 = θcc*xU/delX - xyzULL - xyzLUU + xyzLLU
 
-    #cc_ax1, cc_ax2, cc_ax3, cc_ax4, cc_ax5, cc_ax6 = yzUL, yzUU, yzLL, yzLL, yzLU, yzUU
-    #cc_ay1, cc_ay2, cc_ay3, cc_ay4, cc_ay5, cc_ay6 = xzUL, xzUU, xzLL, xzLU, xzLU, θcc/delY
-    #cc_az1, cc_az2, cc_az3, cc_az4, cc_az5, cc_az6 = xyUU, xyUL, xyLU, xyLL, xyUL, xyLU
+    cc_ax1, cc_ax2, cc_ax3, cc_ax4, cc_ax5, cc_ax6 = yzLL, yzLU, yzUU, yzUU, yzUL, -θcc/delX
+    cc_ay1, cc_ay2, cc_ay3, cc_ay4, cc_ay5, cc_ay6 = xzLL, xzLU, xzUU, xzUL, xzUL, xzLU
+    cc_az1, cc_az2, cc_az3, cc_az4, cc_az5, cc_az6 = xyLL, xyUL, xyUL, xyUU, xyLU, xyLU
 
     cv1 = cv_ax1*x.cv - cv_ay1*y.cc - cv_az1*z.cc + cv_b1
     cv2 = cv_ax2*x.cv + cv_ay2*y.cv + cv_az2*z.cv + cv_b2
@@ -314,12 +313,12 @@ function trilinear_case_9(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
     cv5 = cv_ax5*x.cv + cv_ay5*y.cv - cv_az5*z.cc + cv_b5
     cv6 = cv_ax6*ifelse(cv_ax6 > 0.0, x.cv, -x.cc) - cv_ay6*y.cc - cv_az6*z.cc + cv_b6
 
-    #cc1 = -cc_ax1*x.cv - cc_ay1*y.cv + cc_az1*z.cc + cc_b1
-    #cc2 = -cc_ax2*x.cv - cc_ay2*y.cv - cc_az2*z.cv + cc_b2
-    #cc3 = cc_ax3*x.cc - cc_ay3*y.cv + cc_az3*z.cc + cc_b3
-    #cc4 = cc_ax4*x.cc - cc_ay4*y.cv - cc_az4*z.cv + cc_b4
-    #cc5 = cc_ax5*x.cc - cc_ay5*y.cv - cc_az5*z.cv + cc_b5
-    #cc6 = -cc_ax6*x.cv + cc_ay6*ifelse(cc_ay6 > 0.0, y.cc, -y.cv) + cc_az6*z.cv + cc_b6
+    cc1 = cc_ax1*x.cc + cc_ay1*y.cc + cc_az1*z.cc + cc_b1
+    cc2 = cc_ax2*x.cc + cc_ay2*y.cc - cc_az2*z.cv + cc_b2
+    cc3 = cc_ax3*x.cc - cc_ay3*y.cv - cc_az3*z.cv + cc_b3
+    cc4 = cc_ax4*x.cc - cc_ay4*y.cv - cc_az4*z.cv + cc_b4
+    cc5 = cc_ax5*x.cc - cc_ay5*y.cv + cc_az5*z.cc + cc_b5
+    cc6 = cc_ax6*ifelse(cc_ax6 > 0.0, x.cc, -x.cv) + cc_ay6*y.cc + cc_az6*z.cc + cc_b6
 
     @unpack_trilinear_end()
 end
