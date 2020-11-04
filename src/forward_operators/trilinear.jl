dd1411dd80e0aa581a74db52b21288c2dbbf7683
Matt W <matthew.wilhelm@uconn.edu>
2020-11-04 10:50:56 -0500

    Finish trilinear case 8

--- src/forward_operators/trilinear.jl
+++ src/forward_operators/trilinear.jl
@@ -642,15 +642,13 @@ Case 3.8 + Case 4.8 of Meyer-Floudas 2004
 function trilinear_case_8(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float64}) where {N,T<:RelaxTag}
     @unpack_trilinear_bnd()
 
-    delX = xU - xL
-    delY = yU - yL
-
     # define cv and coefficients
     if (xyzLLL + xyzUUU <= xyzLLL + xyzUUU) &&
        (xyzLLL + xyzUUU <= xyzLUL + xyzULU)
 
-       θcv1 = xyzLLU - xyzUUU - xyzLLL + xyzLUL
-       θcv2 = xyzULU - xyzLLL - xyzUUU + xyzUUL
+        delX = xU - xL
+        θcv1 = xyzLLU - xyzUUU - xyzLLL + xyzLUL
+        θcv2 = xyzULU - xyzLLL - xyzUUU + xyzUUL
 
         cv_b1 = -2.0*xyzLUU
         cv_b2 = -2.0*xyzULL
@@ -672,6 +670,7 @@ function trilinear_case_8(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
 
     else
 
+        delY = yU - yL
         θcv1 = xyzUUU - xyzLLU - xyzUUL + xyzLUL
         θcv2 = xyzLLL - xyzUUL - xyzLLU + xyzULU
 
@@ -684,7 +683,7 @@ function trilinear_case_8(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
 
         cv_ax1, cv_ax2, cv_ax3, cv_ax4, cv_ax5, cv_ax6 = yzUU, yzLL, yzLU, yzUL, yzUL, yzLU
         cv_ay1, cv_ay2, cv_ay3, cv_ay4, cv_ay5, cv_ay6 = xzLU, xzUL, xzUU, xzLL, θcv1/delY, -θcv2/delY
-        cv_az1, cv_az2, cv_az3, cv_az4, cv_az5, cv_az6 = xyUU, xyUL, xyLU, xyLL, xyLL, θcv2/delZ
+        cv_az1, cv_az2, cv_az3, cv_az4, cv_az5, cv_az6 = xyLU, xyUL, xyUU, xyLL, xyUU, xyLL
 
         cv1 = cv_ax1*x.cv - cv_ay1*y.cc - cv_az1*z.cc + cv_b1
         cv2 = cv_ax2*x.cv - cv_ay2*y.cc - cv_az2*z.cc + cv_b2
@@ -694,26 +693,24 @@ function trilinear_case_8(x::MC{N,T}, y::MC{N,T}, z::MC{N,T}, q::Interval{Float6
         cv6 = cv_ax6*x.cv + cv_ay6*ifelse(cv_az6 > 0.0, y.cv, -y.cc) - cv_az6*z.cc + cv_b6
     end
 
-    #θcc = xyzULL - xyzUUU - xyzLLL + xyzLUL
-
     # define cc and coefficients
-    #cc_b1 = -2.0*xyzUUL
-    #cc_b2 = -(xyzULU + xyzULL)
-    #cc_b3 = -(xyzLUU + xyzLLU)
-    #cc_b4 = -(xyzLUU + xyzLUL)
-    #cc_b5 = -(xyzULU + xyzLLU)
-    #cc_b6 = θcc*zU/delZ - xyzULL - xyzLUL + xyzUUU
+    cc_b1 = -(xyzLLU + xyzLLL)
+    cc_b2 = -(xyzLUL + xyzLLL)
+    cc_b3 = -(xyzLLU + xyzULU)
+    cc_b4 = -(xyzUUU + xyzULU)
+    cc_b5 = -(xyzUUU + xyzUUL)
+    cc_b6 = -(xyzLUL + xyzUUL)
 
-    #cc_ax1, cc_ax2, cc_ax3, cc_ax4, cc_ax5, cc_ax6 = yzUL, yzLL, yzUU, yzUU, yzLU, yzLL
-    #cc_ay1, cc_ay2, cc_ay3, cc_ay4, cc_ay5, cc_ay6 = xzUL, xzUU, xzLU, xzLL, xzUU, xzLL
-    #cc_az1, cc_az2, cc_az3, cc_az4, cc_az5, cc_az6 = xyUU, xyUL, xyLL, xyLU, xyLL, -θcc/delZ
+    cc_ax1, cc_ax2, cc_ax3, cc_ax4, cc_ax5, cc_ax6 = yzLL, yzLL, yzLU, yzUU, yzUU, yzUL
+    cc_ay1, cc_ay2, cc_ay3, cc_ay4, cc_ay5, cc_ay6 = xzLU, xzLL, xzLU, xzUU, xzUL, xzUL
+    cc_az1, cc_az2, cc_az3, cc_az4, cc_az5, cc_az6 = xyLL, xyLU, xyUL, xyUL, xyUU, xyLU
 
-    #cc1 = -cc_ax1*x.cv - cc_ay1*y.cv + cc_az1*z.cc + cc_b1
-    #cc2 = -cc_ax2*x.cv + cc_ay2*y.cc + cc_az2*z.cv + cc_b2
-    #cc3 = cc_ax3*x.cc + cc_ay3*y.cc + cc_az3*z.cv + cc_b3
-    #cc4 = cc_ax4*x.cc - cc_ay4*y.cv + cc_az4*z.cc + cc_b4
-    #cc5 = cc_ax5*x.cc + cc_ay5*y.cc + cc_az5*z.cv + cc_b5
-    #cc6 = -cc_ax6*x.cv - cc_ay6*y.cv + cc_az6*ifelse(cc_az6 > 0.0, z.cc, -z.cv) + cc_b6
+    cc1 = cc_ax1*x.cc - cc_ay1*y.cv - cc_az1*z.cv + cc_b1
+    cc2 = cc_ax2*x.cc - cc_ay2*y.cv - cc_az2*z.cv + cc_b2
+    cc3 = cc_ax3*x.cc - cc_ay3*y.cv - cc_az3*z.cv + cc_b3
+    cc4 = cc_ax4*x.cc - cc_ay4*y.cv - cc_az4*z.cv + cc_b4
+    cc5 = cc_ax5*x.cc - cc_ay5*y.cv - cc_az5*z.cv + cc_b5
+    cc6 = cc_ax6*x.cc - cc_ay6*y.cv - cc_az6*z.cv + cc_b6
 
     @unpack_trilinear_end()
 end
