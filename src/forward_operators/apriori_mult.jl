ac91d47712f7849dadad83725e328daee2e8093c
Matthew Wilhelm <matthew.wilhelm@uconn.edu>
2022-03-01 16:52:13 -0500

    Update apriori mult routines

--- src/forward_operators/apriori_mult.jl
+++ src/forward_operators/apriori_mult.jl
@@ -50,10 +50,10 @@ function mult_apriori_kernel(x1::MC{N,T}, x2::MC{N,T}, z::Interval{Float64},
     x2Lx1i = x2Li*x1i
 
     # additional constraints from underestimators (convex)
-    w1cvi = (x2Ui - a2i)*u1i + (x1Ui - a1i)*u2i + lo(a2x1i) + lo(a1x2i) + a1i*a2i - a1i*x2Ui - a2i*x1Ui - APRIORI_DELTA
-    w2cvi = (x2Ui - x2Li)*u1i + lo(x2Lx1i) + lo(a1x2i) - a1i*x2Ui - APRIORI_DELTA
-    w3cvi = (x1Ui - x1Li)*u2i + lo(a2x1i) + lo(x1Lx2i) - x1Ui*a2i - APRIORI_DELTA
-    w4cvi = (a2i - x2Li)*u1i + (a1i - x1Li)*u2i + lo(x2Lx1i) + lo(x1Lx2i) - a1i*a2i - APRIORI_DELTA
+    w1cvi = (x2Ui - a2i)*u1i + (x1Ui - a1i)*u2i + lo(a2x1i) + lo(a1x2i) + a1i*a2i - a1i*x2Ui - a2i*x1Ui
+    w2cvi = (x2Ui - x2Li)*u1i + lo(x2Lx1i) + lo(a1x2i) - a1i*x2Ui
+    w3cvi = (x1Ui - x1Li)*u2i + lo(a2x1i) + lo(x1Lx2i) - x1Ui*a2i 
+    w4cvi = (a2i - x2Li)*u1i + (a1i - x1Li)*u2i + lo(x2Lx1i) + lo(x1Lx2i) - a1i*a2i
 
     w1cv = lo(w1cvi)
     w2cv = lo(w2cvi)
@@ -61,10 +61,10 @@ function mult_apriori_kernel(x1::MC{N,T}, x2::MC{N,T}, z::Interval{Float64},
     w4cv = lo(w4cvi)
 
     # additional constraints from underestimators (concave)
-    w1cci = (x2Li - a2i)*u1i + (a1i - x1Ui)*u2i + hi(a2x1i) + hi(x1Ux2i) - a1i*x2Li + APRIORI_DELTA
-    w2cci = (x2Li - x2Ui)*u1i + hi(a1x2i) + hi(x2Ux1i) - x2Li*a1i + APRIORI_DELTA
-    w3cci = (x1Li - x1Ui)*u2i + hi(a2x1i) + hi(x1Ux2i) - x1Li*a2i + APRIORI_DELTA
-    w4cci = (a2i - x2Ui)*u1i + (x1Li - a1i)*u2i + hi(x2Ux1i) + hi(a1x2i) - x1Li*a2i + APRIORI_DELTA
+    w1cci = (x2Li - a2i)*u1i + (a1i - x1Ui)*u2i + hi(a2x1i) + hi(x1Ux2i) - a1i*x2Li
+    w2cci = (x2Li - x2Ui)*u1i + hi(a1x2i) + hi(x2Ux1i) - x2Li*a1i
+    w3cci = (x1Li - x1Ui)*u2i + hi(a2x1i) + hi(x1Ux2i) - x1Li*a2i
+    w4cci = (a2i - x2Ui)*u1i + (x1Li - a1i)*u2i + hi(x2Ux1i) + hi(a1x2i) - x1Li*a2i
 
     w1cc = hi(w1cci)
     w2cc = hi(w2cci)
@@ -77,7 +77,7 @@ function mult_apriori_kernel(x1::MC{N,T}, x2::MC{N,T}, z::Interval{Float64},
         cv_grad = (x2U - a2)*u1grad + (x1U - a1)*u2grad + grad_temp
     elseif cvind == 2
         @temp_grad_min(x2L, x1cv, x1cc, a1, x2cv, x2cc)
-        cv_grad = (x2U - x2L)*u2grad + grad_temp
+        cv_grad = (x2U - x2L)*u1grad + grad_temp
     elseif cvind == 3
         @temp_grad_min(a2, x1cv, x1cc, x1L, x2cv, x2cc)
         cv_grad = (x1U - x1L)*u2grad + grad_temp
@@ -91,7 +91,7 @@ function mult_apriori_kernel(x1::MC{N,T}, x2::MC{N,T}, z::Interval{Float64},
         @temp_grad_max(a2, x1cv, x1cc, x1U, x2cv, x2cc)
         cc_grad = (x2L - a2)*u1grad + (a1 - x1U)*u2grad + grad_temp
     elseif ccind == 2
-        @temp_grad_max(a1, x1cv, x1cc, x2U, x1cv, x1cc)
+        @temp_grad_max(x2U, x1cv, x1cc, a1, x2cv, x2cc)
         cc_grad = (x2L - x2U)*u1grad + grad_temp
     elseif ccind == 3
         @temp_grad_max(a2, x1cv, x1cc, x1U, x2cv, x2cc)
@@ -100,7 +100,7 @@ function mult_apriori_kernel(x1::MC{N,T}, x2::MC{N,T}, z::Interval{Float64},
         @temp_grad_max(x2U, x1cv, x1cc, a1, x2cv, x2cc)
         cc_grad = (a2 - x2U)*u1grad + (x1L - a1)*u2grad + grad_temp
     end
-
+    
     out = MC{N,T}(cv, cc, z, cv_grad, cc_grad, x1.cnst && x2.cnst)
     return out
 end
