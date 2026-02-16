0781e7e0c51d746fe5091b97f157bff3a3e51b36
Dimitri Alston <123396563+DimitriAlston@users.noreply.github.com>
2026-02-16 12:02:37 -0500

    Update comparison.jl

--- src/forward_operators/no_gradient/comparison.jl
+++ src/forward_operators/no_gradient/comparison.jl
@@ -9,20 +9,6 @@
 # Defines :<, :<=, :!=, :==.
 #############################################################################
 
-for f in (:<, :<=)
-    @eval begin
-        function ($f)(::ANYRELAX, x::MCNoGrad, y::MCNoGrad)
-            ($f)(x.cv,y.cv) && ($f)(x.cc,y.cc) && ($f)(x.Intv, y.Intv)
-        end
-        function ($f)(::ANYRELAX, x::MCNoGrad, c::Float64)
-            ($f)(x.cv, c) && ($f)(x.cc, c) && ($f)(x.Intv, c)
-        end
-        function ($f)(::ANYRELAX, c::Float64, y::MCNoGrad)
-            ($f)(c, y.cv) && ($f)(c, y.cc) && ($f)(c, y.Intv)
-        end
-    end
-end
-
 function <(::ANYRELAX, x::MCNoGrad, y::MCNoGrad) where {N, T<:RelaxTag}
     (x.cv < y.cv) && (x.cc < y.cc) && (isstrictless(x.Intv, y.Intv))
 end
